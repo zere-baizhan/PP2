08fc5cf2c0fcb340d7e807fa8494e9b14d591bcf
Zere <z_baizhan@kbtu.kz>
2024-01-19 12:52:12 +0600

    lab2

---
+++ lab2/arrays.py
@@ -0,0 +1,24 @@
+#ex1
+cars = ["Ford", "Volvo", "BMW"]
+x=cars[0]
+print(x)
+
+#ex2
+cars[0]="Toyota"
+
+#ex3
+x=len(cars)
+print(x)
+
+#ex4
+for x in cars:
+    print(x)
+
+#ex5
+cars.append("Honda")
+
+#ex6
+cars.pop(1)
+
+#ex7
+cars.remove("Volvo")
