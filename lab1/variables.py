4433bacdc77acee111cbaf769672bf0510ad29fe
Zere <z_baizhan@kbtu.kz>
2024-01-19 10:26:32 +0600

    variables

---
+++ lab1/variables.py
@@ -0,0 +1,28 @@
+#ex1
+carname="Volvo"
+
+#ex2
+x=50
+
+#ex3
+x1=5
+y=10
+print(x1+y)
+
+#ex4
+x3=5
+y1=10
+z=x+y
+print(z)
+
+#ex5
+x4,y2,z1="Orange","Banana","Cherry"
+
+#ex6
+x5=y=z="Orange"
+
+#ex7
+def myfunc():
+    global x6
+    x6="fantastic"
+
