7f929d89df8125f670125327b02d853f666efb65
Zere <z_baizhan@kbtu.kz>
2024-03-24 20:58:10 +0500

    lab7

--- lab7/clock.py
+++ lab7/clock.py
@@ -7,9 +7,7 @@ screen = pygame.display.set_mode((screen_width, screen_height))
 clock = pygame.time.Clock()
 
 def blitRotate(surf, image, pos, originPos, angle):
- 
     image_rect = image.get_rect(center=originPos)
-
     rotated_image = pygame.transform.rotate(image, angle)
     rotated_image_rect = rotated_image.get_rect(center=image_rect.center)
     rotated_image_rect.center = pos
@@ -17,14 +15,14 @@ def blitRotate(surf, image, pos, originPos, angle):
 
 
 mainclock = pygame.image.load("mainclock.png")
-minute = pygame.image.load("rightarm.png")
-seconds = pygame.image.load("leftarm.png")
+seconds = pygame.image.load("rightarm.png")  
+minute = pygame.image.load("leftarm.png")
 
 mainclockw, mainclockh = mainclock.get_size()
 
 clockcenter = (screen_width // 2, screen_height // 2)
 
-coordinate_center = (700,530)
+coordinate_center = (700, 530)
 
 done = False
 while not done:
@@ -35,18 +33,16 @@ while not done:
 
     current_time = datetime.datetime.now()
     
-    
-    curr_seconds = current_time.second
-    angle_hours = -(curr_seconds / 60) * 360  
+    curr_seconds = current_time.second + current_time.microsecond / 1000000 
+    angle_seconds = -(curr_seconds / 60) * 360  
 
-    curr_min = current_time.minute
-    angle_minutes = -(curr_min / 60) * 360  
+    curr_min = current_time.minute + curr_seconds / 60  
+    angle_minutes = -(curr_min / 60) * 360 
 
     screen.blit(mainclock, (0, 0))  
 
-    blitRotate(screen, minute, coordinate_center, coordinate_center, angle_hours)
-    
-    blitRotate(screen, seconds, coordinate_center, coordinate_center, angle_minutes)
+    blitRotate(screen, seconds, coordinate_center, coordinate_center, angle_seconds)  
+    blitRotate(screen, minute, coordinate_center, coordinate_center, angle_minutes)
     
     pygame.display.flip()
 
